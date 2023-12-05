2267ba703f1c4728d0ae72aa202471f021fb5508
Dakota Schramm <william.schramm11@gmail.com>
2023-12-05 09:11:50 -0600

    More work to attempt day 3

--- day_three/part_one.py
+++ day_three/part_one.py
@@ -7,70 +7,61 @@ def check_char_type(char):
   elif char in string.punctuation: return "punctuation"
   else:                            return "num"
 
-def parse_number_in_schematic(schematic, type, pos):
-  row, col = pos
+def find_symbol_adjacent_nums(schematic):
+  n = len(schematic)
 
-
-
-def read_schematic(schematic):
-  m = len(schematic)
-  n = len(schematic[0])
-  print(n, m)
-
-  # def check_for_adjacent_numbers(row, column):
-  #   center_space = schematic[row][column]
-  #   num_to_add_for_space = 0
-
-  #   print(row, column, center_space)
-  #   for i in range(row-1, row+2):
-  #     for j in range(column-1, column+2):
-  #       if i == row and j == column: continue
-  #       print(i, j, schematic[i][j])
-
-
-  #       char_type = check_char_type(center_space)
-  #       if char_type == "num":
-  #         if i != row:
-  #           pass
-  #         else:
-  #           num_str = str(center_space)
-  #           if j == column-1:
-  #             current = j
-  #             while -1 < current and check_char_type(schematic[i][current]) == "num":
-  #               num_str = schematic[i][current] + num_str
-  #               current -= 1
-  #           elif j == col+1:
-  #             current = j
-  #             while current < n and check_char_type(schematic[i][current] == "num"):
-  #               num_str = num_str + schematic[i][current]
-  #               current += 1
-
-  #           num_to_add_for_space += int(num_str)
-
-  part_number_sum = 0
-  for line_num, line in enumerate(schematic):
-    cur_num = ""
-
-    if line_num in [0, m-1]: continue
-
-    for col_num, char in enumerate(line):
-      if col_num in [0, n-1]: continue
-      cur_val = schematic[line_num][col_num]
+  adjacent_to_symbol = set()
+  for row, line in enumerate(schematic):
+    for col, char in enumerate(line):
+      cell = (row, col)
+      cur_val = schematic[row][col]
 
       char_type = check_char_type(cur_val)
       if char_type == "num":
-        cur_num += cur_val
+        
+        for r in range( row-1, min(row+2, n) ):
+          for c in range( col-1, min(col+2, n) ):
+            if r == row and c == col: continue
+            if check_char_type( schematic[r][c] ) == "punctuation": adjacent_to_symbol.add(cell) 
   
-  return part_number_sum
-  
+  return adjacent_to_symbol
 
+def calculate_part_num_total(schematic, num_coords):
+  total_part_num = 0
+
+  for row, line in enumerate(schematic):
+    cur_num = ""
+    is_part_num = False
+
+    for col, char in enumerate(line):
+      current_cell = (row, col)
+
+      char_type = check_char_type(char)
+      if char_type == "num":
+        cur_num += char
+        if current_cell in num_coords: is_part_num = True
+      else:
+        if is_part_num:
+          print(cur_num)
+          to_add = int(cur_num)
+          total_part_num += to_add
+        cur_num = ""
+        is_part_num = False
+    
+    cur_num = cur_num.strip("\n")
+    print(cur_num)
+    if cur_num: total_part_num += int(cur_num)
+
+  return total_part_num
+  
 if __name__ == "__main__":
   schematic = []
   with open("day_three.txt") as file:
+  # with open("day_three/one_example.txt") as file:
     for line in file:
       schematic += [line]
   
-  output = read_schematic(schematic)
-  print(output)
-  
-  
+  num_coords = find_symbol_adjacent_nums(schematic)
+  part_num_total = calculate_part_num_total(schematic, num_coords)
+
+  print(part_num_total)
