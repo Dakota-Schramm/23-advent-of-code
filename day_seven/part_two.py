ace273112984749a384b95c2a2305c229739a0c1
Dakota Schramm <william.schramm11@gmail.com>
2023-12-07 23:07:13 -0600

    Add more to attempt 7.

--- day_seven/part_two.py
+++ day_seven/part_two.py
@@ -1,8 +1,26 @@
 from collections import Counter
 from functools import cmp_to_key
 
+HAND_TOTAL = 5
 CARD_ORDER = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']
 FIRST_ORDERING = ["5oak", "4oak", "FH", "3OAK", "2P", "1P", "HC"]
+
+def calculate_wildcard_ordering(card_counts, wildcards, match_counts):
+  if wildcards in [4, 5]:             return FIRST_ORDERING[0]
+
+  if wildcards == 3:
+    if 2 in match_counts:             return FIRST_ORDERING[0]
+    else:                             return FIRST_ORDERING[1]
+  elif wildcards == 2:
+    # print("***2"
+    if 3 in match_counts:             return FIRST_ORDERING[0]
+    elif match_counts.count(2) == 2:  return FIRST_ORDERING[1]
+    else:                             return FIRST_ORDERING[3]
+  elif wildcards == 1:
+    if 4 in match_counts:             return FIRST_ORDERING[0]
+    elif 3 in match_counts:           return FIRST_ORDERING[1]
+    elif 2 in match_counts:           return FIRST_ORDERING[3]
+    else:                             return FIRST_ORDERING[-2]
 
 def calculate_first_ordering(hand_winning):
   # Five of a kind, where all five cards have the same label: AAAAA
@@ -18,35 +36,22 @@ def calculate_first_ordering(hand_winning):
   wildcards = card_counts["J"]
   
   # print(hand, card_counts)
-  if wildcards in [4, 5]:     
-    # print(f"***{wildcards}")
-    return FIRST_ORDERING[0]
 
-  vals = list(card_counts.values())
-  if wildcards == 3:
-    # print("***3")
-    if 2 in vals:             return FIRST_ORDERING[0]
-    else:                     return FIRST_ORDERING[1]
-  elif wildcards == 2:
-    # print("***2")
-    if 3 in vals:             return FIRST_ORDERING[0]
-    elif vals.count(2) == 2:  return FIRST_ORDERING[1]
-    else:                     return FIRST_ORDERING[3]
-  elif wildcards == 1:
-    if 4 in vals:             return FIRST_ORDERING[0]
-    elif 3 in vals:           return FIRST_ORDERING[1]
-    elif 2 in vals:           return FIRST_ORDERING[3]
-    else:                     return FIRST_ORDERING[-2]
+  match_counts = list(card_counts.values())
+  if wildcards:               return calculate_wildcard_ordering(
+                                      card_counts, 
+                                      wildcards,
+                                      match_counts
+                                     ) 
 
   if distinct_cards == 1:     return FIRST_ORDERING[0]
   elif distinct_cards == 2:
-    if 4 in vals:             return FIRST_ORDERING[1]
-    else:                     return FIRST_ORDERING[2]
+    if 4 in match_counts:             return FIRST_ORDERING[1]
+    elif 3 in match_counts:           return FIRST_ORDERING[2]
   elif distinct_cards == 3:
-    if 3 in vals:             return FIRST_ORDERING[3]
-    if vals.count(2) == 2:    return FIRST_ORDERING[4]
-  elif distinct_cards == 4:
-    if 2 in vals:             return FIRST_ORDERING[5]
+    if 3 in match_counts:             return FIRST_ORDERING[3]
+    elif match_counts.count(2) == 2:  return FIRST_ORDERING[4]
+  elif distinct_cards == 4:   return FIRST_ORDERING[5]
   elif distinct_cards == 5:   return FIRST_ORDERING[6]
 
 def calculate_second_ordering(hand_winning1, hand_winning2):
@@ -66,13 +71,13 @@ def calculate_second_ordering(hand_winning1, hand_winning2):
     return val2 - val1
 
 def order_hands(hands):
-  first_ordering_pile = { order:[] for order in FIRST_ORDERING }
+  first_ordering_pile = { order:[] for order in reversed(FIRST_ORDERING) }
   for hand in hands:
     first_ordering_pile[ calculate_first_ordering(hand) ] += [hand]
   print(*first_ordering_pile.items(), sep="\n-----\n")
 
   ordered_hand = []
-  for hand_type, hands_awarded_type in reversed(first_ordering_pile.items()):
+  for hand_type, hands_awarded_type in first_ordering_pile.items():
     # print(hand_type,hands_awarded_type, ordered_hand)
     n = len(hand_type)
     if n == 0: continue
@@ -99,5 +104,5 @@ if __name__ == "__main__":
       winnings += [ int(winning) ]
   # print(hands, winnings)
   total_winnings = calculate_camel_case_winnings(hands, winnings)
-  print(total_winnings)
+  print(249345525 == total_winnings)
   
