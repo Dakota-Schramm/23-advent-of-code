74883c3c1d86dfa40125b6cd2acb2ce9f3de430d
Dakota Schramm <william.schramm11@gmail.com>
2023-12-23 09:29:55 -0500

    Attempt day 22

---
+++ day_twenty_two/part_one.py
@@ -0,0 +1,9 @@
+class Solution:
+  def __init__(self):
+    pass
+
+if __name__ == "__main__":
+  with open("day_twenty_one/example_one.txt") as file:
+  # with open("day_twenty_one.txt") as file:
+    for line in file:
+      print(line)
